d239b4bbe83d36b1b8f9e2bef084ff6cf93846e0
kinkypupecho <subethanwhite@gmail.com>
2023-06-04 14:28:41 +0000

    June 4, 2023

--- text-generation-webui/start_chatbot_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /workspace/text-generation-webui
-python server.py --listen --chat --auto-devices --wbits 4 --groupsize 128 --model_type LLaMa --model TheBloke_manticore-13b-chat-pyg-GPTQ
